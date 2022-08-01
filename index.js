58deda930466027d8c42d05fe2e3ba96b8106b01
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-01 23:52:06 +0300

    db and vs connection, sql "fetch"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import mysql from 'mysql2';
 
 const app = express();
 const port = 8081;
@@ -14,7 +15,22 @@ app.engine('hbs', handlebars.engine({
 app.get('/test', (req, res) => res.render('test'));
 
 
-app.get('/', (req, res) => res.send("hi"));
+// app.get('/', (req, res) => res.send("hi"));
+app.get('/', (req, res) => res.render('index'));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'classicmodels',
+    user: 'classicmodels',
+    password: 'bit'
+})
+
+app.get('/db', (req, res) => {
+    connection.execute('SELECT productLine, textDescription FROM classicmodels.productlines', (err, rows) => {
+        const data = rows.map(row => row);
+        res.render('db', { data: data });
+    });
+})
 
 app.listen(port, () => console.log(`starting server on port ${port}`));
 

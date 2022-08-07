83a323e6c2cd7eae5122be404eb6487a15e5c6c6
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-07 21:10:00 +0300

    insert note through POST

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import mysql from 'mysql2';
-import mainPage from './lib/mainPage.js';
+import { renderMainPage, insertNewNote } from './lib/mainPage.js';
 
 const app = express();
 const port = 8081;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
@@ -17,22 +20,24 @@ app.get('/test', (req, res) => res.render('test'));
 
 
 // app.get('/', (req, res) => res.send("hi"));
-app.get('/', mainPage);
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'classicmodels',
-    user: 'classicmodels',
-    password: 'bit'
-})
-
-app.get('/db', (req, res) => {
-    connection.execute('SELECT productLine, textDescription FROM classicmodels.productlines', (err, rows) => {
-        const data = rows.map(row => row);
-        res.render('db', { data: data });
-    });
-})
+app.get('/', renderMainPage);
+app.post('/', insertNewNote);
 
 app.listen(port, () => console.log(`starting server on port ${port}`));
 
 // SELECT note FROM todoapp.notes n;
+
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     database: 'classicmodels',
+//     user: 'classicmodels',
+//     password: 'bit'
+// })
+
+// app.get('/db', (req, res) => {
+//     connection.execute('SELECT productLine, textDescription FROM classicmodels.productlines', (err, rows) => {
+//         const data = rows.map(row => row);
+//         res.render('db', { data: data });
+//     });
+// })

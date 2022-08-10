e016d90b577d1efe0073d0e1769a488768f37e76
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-10 19:25:53 +0300

    bcrypt install

---
+++ src/user-routes.js
@@ -0,0 +1,19 @@
+//  snd reg page
+export function startRegistration(req, res) {
+
+}
+
+// reg user or redir back to error
+export function handleRegistration(req, res) {
+
+}
+
+// send login page
+export function loginPage(req, res) {
+
+}
+
+// login user or send back on error
+export function handleLogin(req, res) {
+
+}
